04606bc54d18a920d10ec5d82e3e2b6c7e479a46
Michael Tryby <tryby.michael@epa.gov>
2023-09-26 14:38:01 -0400

    Fat binary aka. universal2 build

--- extern/openmp.cmake
+++ extern/openmp.cmake
@@ -6,62 +6,85 @@ if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
     set(CMAKE_INSTALL_PREFIX ${CMAKE_BINARY_DIR}/install CACHE PATH "..." FORCE)
 endif()
 
+# Append local dir to module search path
+list(APPEND CMAKE_MODULE_PATH ${PROJECT_SOURCE_DIR}/cmake/Modules)
+
 
 include(FetchContent)
 
 cmake_policy(SET CMP0135 NEW)
 
+
 FetchContent_Declare(
     OpenMP
     URL
-        https://github.com/llvm/llvm-project/releases/download/llvmorg-14.0.0/openmp-14.0.0.src.tar.xz
+        https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.0/openmp-15.0.0.src.tar.xz
     URL_HASH
-        SHA256=28a1cbdd3dfdd331e4ed2dda2b4477fc418e455c883bd0d1d6acc331118e4688
+        SHA256=7ee0d1c6a0426a34f102aa5c3a2c307807a4885ce347cb51fbc7e5effd6891c6
     OVERRIDE_FIND_PACKAGE
 )
 
-set(OPENMP_STANDALONE_BUILD TRUE)
-set(LIBOMP_INSTALL_ALIASES OFF)
 
-FetchContent_MakeAvailable(
-    OpenMP
+set(OPENMP_STANDALONE_BUILD
+    TRUE
+)
+set(LIBOMP_INSTALL_ALIASES
+    OFF
 )
 
-add_subdirectory(
-    ${CMAKE_BINARY_DIR}/_deps/openmp-src/openmp-14.0.0.src
-    ${CMAKE_BINARY_DIR}/_deps/openmp-build
+FetchContent_MakeAvailable(
+  OpenMP
+)
+
+
+target_compile_options(
+  omp
+    INTERFACE
+        -Xclang
+        -fopenmp
 )
 
 target_include_directories(
-    omp
+  omp
     INTERFACE
-    $<BUILD_INTERFACE:${CMAKE_BINARY_DIR}/_deps/openmp-build/runtime/src>
-    $<INSTALL_INTERFACE:/usr/local/include>
+        $<BUILD_INTERFACE:${CMAKE_BINARY_DIR}/_deps/openmp-build/runtime/src>
+        $<INSTALL_INTERFACE:/include>
 )
 
 
-# Define install locations (will be prepended by install prefix)
- include(GNUInstallDirs)
+# Configure install
+include(
+    GNUInstallDirs
+)
 
 install(
     TARGETS
         omp
     EXPORT
-        openmpTargets
+        OpenMP_Targets
     RUNTIME DESTINATION
         ${BINDIR}
     LIBRARY DESTINATION
         ${LIBDIR}
 )
 
-install(
+# Export from build tree
+export(
     EXPORT
-        openmpTargets
-    DESTINATION
-        "cmake"
+        OpenMP_Targets
+    NAMESPACE
+        OpenMP::
     FILE
-        openmp-config.cmake
+        "${CMAKE_CURRENT_BINARY_DIR}/OpenMP_Targets.cmake"
 )
+
+
+add_library(
+  OpenMP::OpenMP_CXX
+    ALIAS
+        omp
+)
+
 
 set(CPACK_GENERATOR "TGZ")
 set(CPACK_PACKAGE_VENDOR "")
